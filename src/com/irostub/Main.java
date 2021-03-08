ee717f06209f2af176c17bc9322114c797e94f10
irostub <irostub@gmail.com>
2021-03-08 23:36:27 +0900

    Fix dir path

--- src/com/irostub/Main.java
+++ src/com/irostub/Main.java
@@ -1,19 +1,45 @@
 package com.irostub;
 
+import com.irostub.singleton.SingletonEagerInitialization;
+import com.irostub.singleton.SingletonLazyInitialization;
+import com.irostub.singleton.SingletonThreadSafe;
+
 public class Main {
 
     public static void main(String[] args) {
-        SingletonEagerInitialization instance1 = SingletonEagerInitialization.getInstance();
-        instance1.setChecksum(1);
-        SingletonEagerInitialization instance2 = SingletonEagerInitialization.getInstance();
-        if (instance1.getChecksum() == instance2.getChecksum()) {
-            System.out.println("instance1.getChecksum() = " + instance1.getChecksum());
-            System.out.println("instance2.getChecksum() = " + instance2.getChecksum());
-            System.out.println("SingletonEagerInitialization success");
+        SingletonEagerInitialization singletonEagerInitializationInstance1 = SingletonEagerInitialization.getInstance();
+        singletonEagerInitializationInstance1.setChecksum(1);
+        SingletonEagerInitialization singletonEagerInitializationInstance2 = SingletonEagerInitialization.getInstance();
+        if (singletonEagerInitializationInstance1.getChecksum() == singletonEagerInitializationInstance2.getChecksum()) {
+            System.out.println("singletonEagerInitializationInstance1.getChecksum() = " + singletonEagerInitializationInstance1.getChecksum());
+            System.out.println("singletonEagerInitializationInstance2.getChecksum() = " + singletonEagerInitializationInstance2.getChecksum())
+            ;
+            System.out.println("Singleton Eager Initialization success");
         }
-    }
 
-    public static void starter(int select) {
+        SingletonLazyInitialization singletonLazyInitializationInstance1 = SingletonLazyInitialization.getInstance();
+        singletonLazyInitializationInstance1.setChecksum(2);
+        SingletonLazyInitialization singletonLazyInitializationInstance2 = SingletonLazyInitialization.getInstance();
+        if (singletonLazyInitializationInstance1.getChecksum() == singletonLazyInitializationInstance2.getChecksum()) {
+            System.out.println("singletonLazyInitializationInstance1.getChecksum() = " + singletonLazyInitializationInstance1.getChecksum());
+            System.out.println("singletonLazyInitializationInstance2.getChecksum() = " + singletonLazyInitializationInstance2.getChecksum());
+            System.out.println("Singleton Lazy Initialization success");
+        }
 
+
+        SingletonThreadSafe singletonThreadSafe1 = SingletonThreadSafe.getInstance();
+        SingletonThreadSafe singletonThreadSafe2 = SingletonThreadSafe.getInstance();
+        new Thread(() -> {
+            singletonThreadSafe1.setChecksum(2);
+        });
+
+        new Thread(() -> {
+            singletonThreadSafe2.setChecksum(3);
+        });
+        if (singletonThreadSafe1.getChecksum() == singletonThreadSafe2.getChecksum()) {
+            System.out.println("singletonThreadSafe Instance1.getChecksum() = " + singletonThreadSafe1.getChecksum());
+            System.out.println("singletonThreadSafe Instance2.getChecksum() = " + singletonThreadSafe2.getChecksum());
+            System.out.println("Singleton Thread Safe Initialization success");
+        }
     }
 }
