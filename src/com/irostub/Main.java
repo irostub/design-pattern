b576ff42ea91facda3f4bd8cef88204e5c985582
irostub <irostub@gmail.com>
2021-03-11 20:55:30 +0900

    Add strategy pattern design test code

--- src/com/irostub/Main.java
+++ src/com/irostub/Main.java
@@ -4,18 +4,24 @@ import com.irostub.singleton.SingletonEagerInitialization;
 import com.irostub.singleton.SingletonHolderInitialization;
 import com.irostub.singleton.SingletonLazyInitialization;
 import com.irostub.singleton.SingletonThreadSafe;
-
-import java.util.Arrays;
+import com.irostub.strategy.tabs.*;
 
 public class Main {
-
     public static void main(String[] args) {
+        //Singleton(싱글턴) Pattern Test
         System.out.println(SingletonEagerInitialization.getInstance() == SingletonEagerInitialization.getInstance());
-
         System.out.println(SingletonLazyInitialization.getInstance() == SingletonLazyInitialization.getInstance());
-
         System.out.println(SingletonThreadSafe.getInstance() == SingletonThreadSafe.getInstance());
-
         System.out.println(SingletonHolderInitialization.getInstance() == SingletonHolderInitialization.getInstance());
+
+        //Strategy(전략) Pattern Test
+        Tab tab = new Tab();
+        tab.onClick();
+        tab.setStrategyTab(new TabShop());
+        tab.onClick();
+        tab.setStrategyTab(new TabMail());
+        tab.onClick();
+        tab.setStrategyTab(new TabCafe());
+        tab.onClick();
     }
 }
