f38b6469048751dd92389bf2adfa44a9732ebf8d
irostub <irostub@gmail.com>
2022-03-15 16:23:16 +0900

    Add singleton example with spring framework

---
+++ src/main/java/com/irostub/designpatterns/singleton/Settings5.java
@@ -0,0 +1,10 @@
+package com.irostub.designpatterns.singleton;
+
+/**
+ * Enum을 사용해서 싱글톤 만들기
+ */
+public enum Settings5 {
+
+    INSTANCE;
+
+}
---
+++ src/main/java/com/irostub/designpatterns/singleton/SpringConfig.java
@@ -0,0 +1,14 @@
+package com.irostub.designpatterns.singleton;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public String hello() {
+        return "hello";
+    }
+
+}
