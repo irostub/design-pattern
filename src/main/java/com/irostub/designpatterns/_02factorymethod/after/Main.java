847aca13458bdb6e1838a5971842c1aadd7fff7e
irostub <irostub@gmail.com>
2022-03-29 21:46:39 +0900

    Fix main code, used like interface method

--- src/main/java/com/irostub/designpatterns/_02factorymethod/after/Main.java
+++ src/main/java/com/irostub/designpatterns/_02factorymethod/after/Main.java
@@ -2,10 +2,16 @@ package com.irostub.designpatterns._02factorymethod.after;
 
 public class Main {
     public static void main(String[] args) {
-        Item item1 = new CandyItemFactory().buildItem();
+        print(new CandyItemFactory());
+        print(new PotionItemFactory());
+
+        /*Item item1 = new CandyItemFactory().buildItem();
         Item item2 = new PotionItemFactory().buildItem();
 
         System.out.println(item1);
-        System.out.println(item2);
+        System.out.println(item2);*/
+    }
+    public static void print(ItemFactory itemFactory) {
+        System.out.println(itemFactory.buildItem());
     }
 }
